44ee7fff5d6d94b5f77030e1325e803ac66d4cf8
markvdesign <markvdesign@gmail.com>
2017-11-14 20:38:14 +1000

    WIP on Hygro

---
+++ resources/hygro.js
@@ -0,0 +1,16 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+  var thermometer = new five.Thermometer({
+    controller: "DHT21_I2C_NANO_BACKPACK"
+  });
+
+  thermometer.on("change", function() {
+    console.log("Thermometer");
+    console.log("  celsius           : ", this.celsius);
+    console.log("  fahrenheit        : ", this.fahrenheit);
+    console.log("  kelvin            : ", this.kelvin);
+    console.log("--------------------------------------");
+  });
+})
