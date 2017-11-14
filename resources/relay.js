444a02dd4abf0bdba6c0e73bcb85276552f414e3
markvdesign <markvdesign@gmail.com>
2017-11-14 20:37:25 +1000

    Update the relay resource

--- resources/relay.js
+++ resources/relay.js
@@ -1,8 +1,8 @@
 const five = require('johnny-five');
 const board = new five.Board();
 
-board.on('ready', () => {
-    const relay = new five.Relay(28);
+board.on('ready', function() {
+    const relay = new five.Relay(22);
 
     console.log(`What type of relay are you? ${relay.type}`);
 
@@ -11,25 +11,18 @@ board.on('ready', () => {
     setInterval(() => {
         relay.toggle();
         console.log(`Is the relay on?  ${relay.isOn}`);
-    }, 5000);
-
+    }, 1000);
+    
     // Read, Eval, Print, Loop allows us to do some basic functionality
     // from the command line
     this.repl.inject({
-        toggle: function(){
-            relay.toggle();
-        },
-        open: function() {
-            relay.open();
-        },
-        close: function() {
-            relay.close();
-        }
+        relay: relay
     });
+
 });
 
 board.on('exit', () => {
-    const relay = new five.Relay(28);
+    const relay = new five.Relay(22);
 
     relay.open();
 });
